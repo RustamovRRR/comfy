4cf67ae600c18d9eecc66f07f544d5c55d3c1606
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-02 21:52:15 +0400

    fetch single product and handle error 404

--- src/pages/product.js
+++ src/pages/product.js
@@ -1,24 +1,44 @@
 // global imports
-import '../toggleSidebar.js';
-import '../cart/toggleCart.js';
-import '../cart/setupCart.js';
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 // specific
-import { addToCart } from '../cart/setupCart.js';
-import { singleProductUrl, getElement, formatPrice } from '../utils.js';
+import { addToCart } from "../cart/setupCart.js";
+import { singleProductUrl, getElement, formatPrice } from "../utils.js";
 
 // selections
-// const loading = getElement('.page-loading');
-// const centerDOM = getElement('.single-product-center');
-// const pageTitleDOM = getElement('.page-hero-title');
-// const imgDOM = getElement('.single-product-img');
-// const titleDOM = getElement('.single-product-title');
-// const companyDOM = getElement('.single-product-company');
-// const priceDOM = getElement('.single-product-price');
-// const colorsDOM = getElement('.single-product-colors');
-// const descDOM = getElement('.single-product-desc');
-// const cartBtn = getElement('.addToCartBtn');
+const loading = getElement(".page-loading");
+const centerDOM = getElement(".single-product-center");
+const pageTitleDOM = getElement(".page-hero-title");
+const imgDOM = getElement(".single-product-img");
+const titleDOM = getElement(".single-product-title");
+const companyDOM = getElement(".single-product-company");
+const priceDOM = getElement(".single-product-price");
+const colorsDOM = getElement(".single-product-colors");
+const descDOM = getElement(".single-product-desc");
+const cartBtn = getElement(".addToCartBtn");
 
 // cart product
 // let productID;
 
 // show product when page loads
+window.addEventListener("DOMContentLoaded", async function () {
+  const urlId = window.location.search;
+  try {
+    const response = await fetch(`${singleProductUrl}${urlId}`);
+    if (response.status >= 200 && response.status <= 299) {
+      const product = await response.json();
+      console.log(product);
+    } else {
+      console.log(response.status, response.statusText);
+      centerDOM.innerHTML = `<div>
+      <h3 class=""error> sorry, something went wrong ${response.status}</h3>
+      <a href="index.html" class="btn">back home</a>
+      </div>`;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  loading.style.display = `none`;
+});
