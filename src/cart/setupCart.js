630e9dd09a8ab60250a78a04d86b7dc6a396ce5d
RustamovRRR <rustamrrustamov@gmail.com>
2022-01-02 23:42:22 +0400

    setup cart 346

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -9,8 +9,27 @@ import { openCart } from "./toggleCart.js";
 import { findProduct } from "../store.js";
 import addToCartDOM from "./addToCartDOM.js";
 // set items
+const cartItemCountDOM = getElement(".cart-item-count");
+const cartItemsDOM = getElement(".cart-items");
+const cartTotalDOM = getElement(".cart-total");
+
+let cart = getStorageItem("cart");
 
 export const addToCart = (id) => {
-  console.log(id);
+  let item = cart.find((cartItem) => cartItem.id === id);
+  if (!item) {
+    let product = findProduct(id);
+    console.log(product);
+  } else {
+    // update values
+  }
+
+  // more stuff coming up
   openCart();
 };
+
+const init = () => {
+  console.log(cart);
+};
+
+init();
